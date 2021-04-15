3929383bc5c57ee114c839cef5c17bfc61f08370
kicknback <joshua.borreli@gmail.com>
2021-04-15 14:48:50 -0500

    operator/variable exercises

---
+++ js/types-operators.js
@@ -0,0 +1,90 @@
+"use strict";
+
+// let isTrue = true;
+// console.log("The value of isTrue: " + isTrue);  //boolean literal
+
+//-------------------------------------  Boolean
+
+
+// let iAmHungry = true;
+//
+// if (iAmHungry) {
+//     console.log("Let's find some fooooood fam!");
+// }
+//
+// let hoursSinceMeal = 5;
+//
+// let amHungry = hoursSinceMeal > 4
+//
+// if (amHungry) {
+//     console.log("I'm getting a little hungry");
+// }
+// //-------------------------------------  Numbers
+//
+// console.log("The number is: " + 10);
+// console.log("The number is: " + 235075073);
+// console.log("The number is: " + -1798);
+//
+// console.log(10 + 10);
+// console.log(2 - 8);
+// console.log(4 * 5);
+// console.log(1 / 0);
+// console.log(-0 / 0);
+// console.log(12 % 3);
+
+// MINI exercise
+
+// TODO: console.log() five equations using each arithmetic operator.  Each equation should resolve to 4.
+
+// console.log(2 + 2);
+// console.log(8 - 4);
+// console.log(2 * 2);
+// console.log(16 / 4);
+// console.log(20 % 8);
+
+// Undefined
+
+// let name;
+//
+// console.log(name);
+//
+// //---------------------------------------- Strings
+//
+// console.log("Casey");
+//
+// console.log("Casey\nis\nhungry.")
+//
+// name = "Casey";
+//
+// console.log(name + "\nIs\nHungry");
+
+// --------------------------------------  Typeof
+
+console.log("The type is: " + typeof(null));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
