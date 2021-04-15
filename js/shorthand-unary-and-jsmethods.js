aad3bb905255e601a4200684a4ede94d21bc3116
‘kicknback’ <joshua.borreli@gmail.com>
2021-04-15 16:04:39 -0500

    adding shorthand jsmethods

---
+++ js/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,56 @@
+"use strict";
+
+//  Shorthand assignment operators
+
+// += / -=
+
+let name = "Casey";
+
+// name = name + " " + "Edwards";
+// console.log(name);
+
+name += " " + "Edwards";
+console.log(name);
+
+let daysRemainingInCohort= 114;
+
+daysRemainingInCohort -= 4;
+
+console.log(daysRemainingInCohort);
+
+// MINI exercise
+
+// TODO: Refactor the mini exercise we did earlier on arithmetic operators to use variables and their related shorthand assignment operators, then log out the result
+//  ie:
+//     var result = 5;
+//     console.log(result += 1);
+
+let num = 2;
+
+console.log(num += 2);
+num = 8;
+console.log(num -= 4);
+num = 2;
+console.log(num *= 2);
+num = 16;
+console.log(num /= 4);
+num = 20;
+console.log(num %= 8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
