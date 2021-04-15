6e8b3d2eb68c7910c44ac34cfa5af9498aaa112b
‘kicknback’ <joshua.borreli@gmail.com>
2021-04-15 15:24:11 -0500

    operator, primitive type, and variable practice

---
+++ js/operatorspractice.js
@@ -0,0 +1,41 @@
+"use strict";
+/*
+'1' + 2
+
+typeof '1' + 2
+
+6 % 4
+
+'3 + 4 is ' + 3 + 4
+// how could you make this produce the correct output?
+
+0 < 0
+
+'false' == false
+
+true == 'true'
+
+5 >= -5
+
+!false || false
+
+true || "42"
+
+!true && !false
+
+6 % 5
+
+5 < 4 && 1 === 1
+
+typeof 'codeup' === 'string'
+
+'codeup' === 'codeup' && 'codeup' === 'Codeup'
+
+4 >= 0 && 1 !== '1'
+
+6 % 3 === 0
+
+5 % 2 !== 0
+*/
+
+
