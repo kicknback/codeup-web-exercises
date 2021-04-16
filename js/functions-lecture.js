a15d2abecc1eb87839cedabaa704a1a2858b3c58
‘kicknback’ <joshua.borreli@gmail.com>
2021-04-16 12:18:20 -0500

    adding functions practice

---
+++ js/functions-lecture.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// MINI EXERCISE
+// TODO: Create functions which will return the product (*) of two numbers and the dividend (/) of two numbers, respectively
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// num1 is to be divided by num2
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(multiply(43, 96));
+console.log(divide(49000, 65));
