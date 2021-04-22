df941c2c983acb0dc2f06f87ff05162568bd01fb
kicknback <joshua.borreli@gmail.com>
2021-04-22 16:47:27 -0500

    adding TODO exercise in loop.js, and java.codeup exercise within while.js

--- js/loop.js
+++ js/loop.js
@@ -1,11 +1,11 @@
 (function(){
     "use strict";
-    let num = 8;
-    while (num <= 10) {
-        console.log('num: ' + num);
-        num += 1;
-        console.log("num plus one:" + num);
-    }
+    // let num = 8;
+    // while (num <= 10) {
+    //     console.log('num: ' + num);
+    //     num += 1;
+    //     console.log("num plus one:" + num);
+    // }
 
     // // TODO: MINI EXERCISES
     // // TODO: print all even numbers between 0 and 25;
@@ -14,25 +14,50 @@
     // hint: start by defining the correct password and don't forget to link your html file
 
 
-    let numIter = 0;
-    while (numIter < 25) {
-        if (numIter % 2 === 0) {
-            console.log("Even number: " + numIter);
-        }
-        numIter++;
-    }
+    // let numIter = 0;
+    // while (numIter < 25) {
+    //     if (numIter % 2 === 0) {
+    //         console.log("Even number: " + numIter);
+    //     }
+    //     numIter++;
+    // }
+    //
+    // let correctPass = 'blahblah';
+    //
+    // while (true) {
+    //     let pass = prompt("What is the password?");
+    //     if (pass === correctPass) {
+    //         alert("That is the correct password.  You are now allowed in...");
+    //         break;
+    //     } else {
+    //         alert("That was not the proper password");
+    //     }
+    // }
+    // //------- do while loop
+    //
+    // // sing happy bday every year and when they reach 16 say "i got you a car"
+    //
+    // let age = 0;
+    //
+    // do {
+    //     console.log(`Happy bday! You're now ${age} years old`);
+    //     age++
+    // }
 
-    let correctPass = 'blahblah';
+    // // TODO: A parking lot with 30 parking spaces wants to display the amount of spaces available as it gets filled.
+    //  Let's assume that there are allowing two cars into the parking lot at a time. As those cars take up spaces,
+    //  write a do-while loop that prints the spaces available. If the parking lot gets filled, print "this parking lot
+    //  is now full" to the console.
+    //  HINT: think of a countdown, remember if statements can be used inside of your statements
 
-    while (true) {
-        let pass = prompt("What is the password?");
-        if (pass === correctPass) {
-            alert("That is the correct password.  You are now allowed in...");
-            break;
-        } else {
-            alert("That was not the proper password");
-        }
-    }
+    let availSpaces = 30;
+
+    do {
+        console.log(`There are currently ${availSpaces} spaces available in our parking lot.`);
+        availSpaces -= 2;
+    } while (availSpaces !== 0);
+
+    console.log("--This parking lot is now full--");
 
 
 
--- js/while.js
+++ js/while.js
@@ -1,11 +1,28 @@
 (function(){
     "use strict";
 
-    let num = 2;
-    while (num < 66000) {
-        console.log(num);
-        num *= 2;
-    }
+    // let num = 2;
+    // while (num < 66000) {
+    //     console.log(num);
+    //     num *= 2;
+    // }
+    // do while exercise
 
+    let coneStock = Math.floor(Math.random() * 50) + 50;
+    console.log(`The amount of cones we need to sell today is ${coneStock}`);
+
+    do {
+        console.log(`We have ${coneStock} ice cream cones left in inventory`);
+        let conesToBuy = Math.floor(Math.random() * 5) + 1;
+        console.log(`I'll take ${conesToBuy} ice cream cones, please`)
+        if (conesToBuy > coneStock) {
+            console.log(`Sorry, can't sell you that many cones because I only have ${coneStock} left`);
+        } else {
+            console.log(`Here are your ${conesToBuy} cones`);
+            coneStock -= conesToBuy;
+        }
+        console.log("\n---------------------------\n")
+    } while (coneStock !== 0);
+    console.log(`Sweet!! sold all my cones and I'm outta here...`);
 
 })()
