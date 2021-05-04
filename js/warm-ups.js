c263dfa343570af9ade0d2450419435fc8b19b99
kicknback <joshua.borreli@gmail.com>
2021-05-04 09:16:41 -0500

    Daily warmup push

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -5,7 +5,18 @@
         return input.trim();
     }
 
-    console.log(trim("    oiwne   "));
+    //console.log(trim("    oiwne   "));
+
+    //----------------------------
+
+    let cube = num => num**3;
+
+    let cubeRoot = num => Math.cbrt(num);
+
+    console.log(cube(3));
+    console.log(cubeRoot(27));
+
+    //-----------------------------
 
 
 })()
