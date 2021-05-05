a0c30dfc5f488fc72059e691ea3d83739348b4b2
kicknback <joshua.borreli@gmail.com>
2021-05-05 09:29:27 -0500

    Adding daily warmup exercise

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -13,11 +13,18 @@
 
     let cubeRoot = num => Math.cbrt(num);
 
-    console.log(cube(3));
-    console.log(cubeRoot(27));
-    console.log(cubeRoot(cube(3)));
+    // console.log(cube(3));
+    // console.log(cubeRoot(27));
+    // console.log(cubeRoot(cube(3)));
 
     //-----------------------------
+
+    let last = input => input[input.length - 1];
+
+    console.log(last("fkvnseilrgheiurghsklhv"));
+
+
+
 
 
 })()
