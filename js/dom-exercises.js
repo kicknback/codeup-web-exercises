bba0b5ec888ec2f976063852a370a134ba5890ea
kicknback <joshua.borreli@gmail.com>
2021-05-19 10:27:29 -0500

    Dom exercise html and js files

---
+++ js/dom-exercises.js
@@ -0,0 +1,18 @@
+(function(){
+    "use strict";
+
+    // // TODO: build a simple 2 x 3 grid using bootstrap and give each
+    //  item a unique border color. Give any three of those of those divs a
+    //  blue background-color. Place a paragraph tag inside of all 6 divs
+    //  (use lorem). Four of the six paragraphs should have dark-green font
+    //  color
+    // TODO: print the following items to the console: 1) A node that
+    //  represents a div/item with a red border. 2) an array of elements
+    //  whose text is dark-green. 3) an array of all paragraph elements
+
+
+
+
+
+
+})()
