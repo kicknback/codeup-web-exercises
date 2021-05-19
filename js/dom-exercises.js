b6dfa243981a59e6a217b85312dd51fd58aa5e6c
kicknback <joshua.borreli@gmail.com>
2021-05-19 11:03:30 -0500

    Dom exercise html/js/css files

--- js/dom-exercises.js
+++ js/dom-exercises.js
@@ -9,9 +9,13 @@
     // TODO: print the following items to the console: 1) A node that
     //  represents a div/item with a red border. 2) an array of elements
     //  whose text is dark-green. 3) an array of all paragraph elements
+    let redBorder = document.getElementById("red-border")
+    let greenItems = document.getElementsByClassName("dark-green");
+    let paras = document.getElementsByTagName("p");
 
-
-
+    console.log(redBorder);
+    console.log(greenItems);
+    console.log(paras);
 
 
 
