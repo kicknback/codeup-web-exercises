45ed1c0a1c02af0c15e5dcee336b8e5dd1798712
kicknback <joshua.borreli@gmail.com>
2021-05-20 11:39:08 -0500

    Adding dom exercise todos

--- js/dom-exercises.js
+++ js/dom-exercises.js
@@ -14,9 +14,52 @@
     let greenItems = document.getElementsByClassName("dark-green");
     let paras = document.getElementsByTagName("p");
 
-    console.log(redBorder);
-    console.log(greenItems);
-    console.log(paras);
+    // console.log(redBorder);
+    // console.log(greenItems);
+    // console.log(paras);
+
+    let newDiv = document.createElement("div");
+    let newElement = document.createElement("ul");
+
+    newDiv.innerText = "Creating a new div";
+    // console.log(newDiv);
+
+    for (let i = 0; i <= 4; i++) {
+        newElement.appendChild(document.createElement("li"));
+    }
+    console.log(newElement);
+    // console.log("%cMy new element is ", "color: red; font-weight: bold; text-decoration: underline");
+
+    // newElement.innerText = "New list";
+    // newElement.innerHTML = "<p>Howdy there</p>";
+
+    let domDiv = document.getElementsByClassName("container-fluid");
+    let body = document.getElementsByTagName("body");
+    body[0].appendChild(newElement);
+
+
+
+    // TODO: change the text inside of 1 of the paragraphs to be a short paragraph about you
+    // TODO: log the second paragraph element from the list of all
+    //  paragraphs
+    // TODO: create a new ul element and 5 new li elements. Each of your li's should have some text inside of it
+    // TODO: replace the last div's paragraph with your new ul. make sure the ul has all 5 li's inside of it
+    // TODO: remove the last li
+
+    let innerDiv = document.getElementsByClassName("col border dark-green");
+    console.log(innerDiv);
+    innerDiv[0].innerHTML = `<p>This is the new paragraph about me.  I'm awesome</p>`;
+    console.log(document.getElementsByTagName("p")[1]);
+
+
+    let items = newElement.getElementsByTagName("li");
+    items[0].innerText = "Pizza";
+    items[1].innerText = "Pasta";
+    items[2].innerText = "Mac";
+    items[3].innerText = "Cous Cous";
+    items[4].innerText = "Bread";
+
+
 
 
 
