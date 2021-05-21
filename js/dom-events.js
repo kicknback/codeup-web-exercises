fbb11a0c07ce0342697ab475b4e4370ea74c274b
kicknback <joshua.borreli@gmail.com>
2021-05-21 14:55:20 -0500

    Updating Dom events blog exercise

--- js/dom-events.js
+++ js/dom-events.js
@@ -1,10 +1,21 @@
 
 function addButtonEvent() {
     document.getElementById("submitBtn")
-            .addEventListener("click", addPost)
+            .addEventListener("click", addPost);
 }
 
 function addPost() {
-
+    let theDate = new Date();
+    if (document.getElementById("user-input").value !== "") {
+        let replicatePost = document.createElement("div");
+        replicatePost.setAttribute("class", "border text-light bg-dark text-wrap");
+        replicatePost.setAttribute("id", "new-post");
+        replicatePost.textContent = document.getElementById("user-input").value + `\n\n${theDate}`;
+        document.getElementById("blog-container").appendChild(replicatePost);
+    } else {
+        alert("Please do not leave blank. Input your post");
+    }
 
 }
+
+addButtonEvent();
