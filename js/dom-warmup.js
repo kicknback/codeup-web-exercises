839ccf2e8b482663fbafccac0e7712152b546941
kicknback <joshua.borreli@gmail.com>
2021-06-03 10:58:28 -0500

    Updating dom warmup

--- js/dom-warmup.js
+++ js/dom-warmup.js
@@ -5,9 +5,18 @@
     let input = $(".form-control");
     let container = $("#post-container");
 
-    button[0].addEventListener("click", function(){
-        container.append(`<div style="font-weight: bold; font-size: 30px">${input[0].value}</div>`);
-    });
+    button[0].addEventListener("click", myFunction);
+
+    function myFunction() {
+        let newDiv = document.createElement("div");
+        // newDiv.setAttribute("class", "col-12");
+        let newGif = document.createElement("img");
+        newGif.setAttribute("src", "images/braveheart.webp")
+        newDiv.append(newGif);
+        container.append(`<div class="" style="font-weight: bold; font-size: 30px">${input[0].value}</div>`);
+        container.append(newDiv);
+        button[0].removeEventListener("click", myFunction);
+    }
 
 
 
