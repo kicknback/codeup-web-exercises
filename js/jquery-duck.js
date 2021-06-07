5399681595b31ece95b3106431c5f5f9caf4104a
kicknback <joshua.borreli@gmail.com>
2021-06-07 11:44:58 -0500

    Completing TODO exercises

--- js/jquery-duck.js
+++ js/jquery-duck.js
@@ -30,54 +30,76 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 //make sure the DOM is loaded first
-$(document).ready(addListeners);
-
-function addListeners() {
-    let ducky = "images/jazz-music-rubber-duck.jpg";
-    let question = "images/question.png";
-    let cardContainers = $(".col-md-3");
-    let cardImage = $(".card-img-top");
-
-    function hoverIn(e) {
-        $(this).find(cardImage).attr("src", ducky);
-    }
-    function hoverOut(e) {
-        $(this).find(cardImage).attr("src", question);
-    }
-    cardContainers.hover(hoverIn, hoverOut);
-}
+// $(document).ready(addListeners);
+//
+// function addListeners() {
+//     let ducky = "images/jazz-music-rubber-duck.jpg";
+//     let question = "images/question.png";
+//     let cardContainers = $(".col-md-3");
+//     let cardImage = $(".card-img-top");
+//
+//     function hoverIn(e) {
+//         $(this).find(cardImage).attr("src", ducky);
+//     }
+//     function hoverOut(e) {
+//         $(this).find(cardImage).attr("src", question);
+//     }
+//     cardContainers.hover(hoverIn, hoverOut);
+// }
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-// TODO: When the user clicks the button with the id of 'submitBtn,' the background color of #override-bootstrap changes to 'red'
+// // TODO: When the user clicks the button with the id of 'submitBtn,' the background color of #override-bootstrap changes to 'red'
+//
+// $("#submitBtn").click(function (){
+//     $("#override-bootstrap").css("background-color", "red");
+// })
+//
+// // TODO: When the user clicks a .card-text element, change its background color to 'red'
+//
+// $(".card-text").click(function(){
+//     $(this).css("background-color", "red");
+// })
+//
+// // TODO: Add a new button to the html, directly below the first button. Give it an id of 'resetBtn'.
+// // -> When the user clicks that button, the page should reload. (HINT: BOM has a super easy method for reloading the page. You'll still need a click event)
+//
+// $("#resetBtn").click(function(){
+//     location.reload();
+// })
+//
+// // TODO: When the user clicks the element with the id #main-title, its text font size doubles
+//
+// $("#main-title").click(function(){
+//     $(this).css("font-size", "400%");
+// })
 
-$("#submitBtn").click(function (){
-    $("#override-bootstrap").css("background-color", "red");
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// TODO: Add a data-attribute to each card container (.col-md-3)
+//  -> Give that data-attribute a value of 1-4 (moving top to bottom in the html)
+//  -> Now, when a user hovers over one of the .col-md-3 containers, you should replace the text inside #card-title with the data-attribute value
+//  -> When the user hovers out, the data-attribute value should be replaced with the original text ("Card")
+
+let hoverIn = () => {
+    $(this).find(".card-title").text($(this).attr("data-attr"));
+}
+
+let hoverOut = () => {
+    $(this).find(".card-title").text("Card");
+}
+
+$(".col-md-3").hover(hoverIn, hoverOut);
+
+// TODO: When an li with the .list-group-item class is clicked, replace the text with the same text, but uppercased
+//  -> ALSO, change the background of the li to a new color
+//  -> When the li is clicked again, reset the li background color to its original color
+//      -> Don't worry about changing the text back
+
+$(".list-group-item").click(function(){
+    $(this).toggleClass("text-uppercase");
+    $(this).toggleClass("bg-danger");
 })
-
-// TODO: When the user clicks a .card-text element, change its background color to 'red'
-
-$(".card-text").click(function(){
-    $(this).css("background-color", "red");
-})
-
-// TODO: Add a new button to the html, directly below the first button. Give it an id of 'resetBtn'.
-// -> When the user clicks that button, the page should reload. (HINT: BOM has a super easy method for reloading the page. You'll still need a click event)
-
-$("#resetBtn").click(function(){
-    location.reload();
-})
-
-// TODO: When the user clicks the element with the id #main-title, its text font size doubles
-
-$("#main-title").click(function(){
-    $(this).css("font-size", "400%");
-})
-
-
-
-
-
 
 
 
