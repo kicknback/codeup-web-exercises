55196ba20ed03eea64607b14f2c1309bef43dc6b
kicknback <joshua.borreli@gmail.com>
2021-06-08 10:45:53 -0500

    Initializing ajax lecture file

---
+++ js/ajax-lecture.js
@@ -0,0 +1,23 @@
+(function(){
+    "use strict";
+
+    // TODO: create a JSON object about your favorite hobby. make sure to include at least one of every data type that can
+    //  be accepted as values
+
+    // {
+    //     "hobby": "hanging out with kids",
+    //     "the best?": true,
+    //     "location": ["the park", "the house", "the couch"],
+    //     "frequency": 8,
+    //     "who": [{
+    //         girl: true,
+    //          youngest: true,
+    //     }, {
+    //         boy: true,
+    //         oldest: true
+    //     }]
+    // }
+
+
+
+})()
