5a47cf4142d1c7ce0ece3961fad92c25cc2c0e26
kicknback <joshua.borreli@gmail.com>
2021-06-09 11:25:28 -0500

    Updating mapbox-playground exercise

--- js/mapbox-playground.js
+++ js/mapbox-playground.js
@@ -1,5 +1,25 @@
 const mapboxToken = "pk.eyJ1Ijoia2lja25iYWNrIiwiYSI6ImNrcHBuOWEwcjJveTIydXBlYzI0M2ttYjcifQ.XQ3-Mc4BInwSCGwala81DA"
 
+mapboxgl.accessToken = mapboxToken;
+
 var map = new mapboxgl.Map({
-    container: "map"
-})
+    container: "map",
+    style: "mapbox://styles/mapbox/dark-v10",
+    center: [-97.1081, 32.7357],
+    zoom: 12
+});
+
+let marker = setMarker([-97.1081, 32.7357]);
+addMapEvent(marker);
+
+function setMarker(point) {
+    return new mapboxgl.Marker().setLngLat(point).addTo(map);
+
+}
+
+function addMapEvent(marker) {
+    map.on('click', function(e){
+        marker.setLngLat(e.lngLat).addTo(map);
+        console.log(e.lngLat);
+    })
+}
