5488163ea3acbce028aaa80819aaba4bf4646dfd
kicknback <joshua.borreli@gmail.com>
2021-06-15 16:42:01 -0500

    Updating promise exercise

--- js/promises-fetch-exercises.js
+++ js/promises-fetch-exercises.js
@@ -1,2 +1,9 @@
 
+fetch("https://dog.ceo/api/breeds/image/random").then(message => {
+    console.log(message)
+    return message.json();
+}).then(data => {
+    $("#random-dog-image").attr("src", data.message);
 
+})
+    .catch(err => console.log(`There was an error of: ${err}`));
